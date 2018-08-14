3abd9444e2afc5bc43a76febb5e427f7a6fc9201
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-08-14 06:19:14 -0300

    Pequenas alterações

--- script.js
+++ script.js
@@ -19,8 +19,10 @@ var MutaçãoMin = 0;
 var MutaçãoMax = 100;
 var soma = 0;
 var muta = 0.01;
-trigger = false;
-taxa = 0.01;
+var pai_index = 0;
+var mae_index = 0;
+var trigger = false;
+var taxa = 0.01;
 
 
 
@@ -29,8 +31,9 @@ String.prototype.replaceAt = function(index, character) {
 };
 
 function taxa_dinamica(tax) {
-  var tamanho_pensamento = pensamento.length;
-  var geracao_new = geracao / 10;
+  let pulo = null;
+  let tamanho_pensamento = pensamento.length;
+  let geracao_new = geracao / 10;
 
   if (tamanho_pensamento > 1 && tamanho_pensamento < (geracao_new) && Number.isInteger(geracao_new)) {
     if ((pensamento[geracao_new - 3].mediafit - pensamento[geracao_new - 2].mediafit) > 0) {
@@ -43,56 +46,52 @@ function taxa_dinamica(tax) {
       this.mediafit = mediafit;
     };
 
-    elemento_pensamento = new lembranca;
+    var elemento_pensamento = new lembranca;
     soma /= 10;
     elemento_pensamento.mediafit = soma;
     soma = 0;
-    pulo = null;
-    escolha = int(random(0, 2));
-    console.log("Teste");
+
+    let escolha = int(random(0, 2));
+    
     if (escolha == 0 || pulo == 0) {
       elemento_pensamento.acao = 0;
       pensamento.push(elemento_pensamento);
       futuro = random(tax - (tax * 2), tax - 0.001);
-      console.log("Acho que preciso diminuir a mutação");
-      console.log(futuro);
-      muta = abs(futuro);
+    //  console.log("Acho que preciso diminuir a mutação");
+      //console.log(futuro);
+      muta = parseFloat(abs(futuro).toFixed(3));
       return futuro;
     }
     if (escolha == 1 || pulo == 1) {
       elemento_pensamento.acao = 1;
       pensamento.push(elemento_pensamento);
       futuro = random(tax + 0.001, tax + (tax * 2));
-      console.log("Acho que preciso aumentar a mutação");
-      console.log(futuro);
-      muta = abs(futuro);
+      //console.log("Acho que preciso aumentar a mutação");
+      //console.log(futuro);
+      muta = parseFloat(abs(futuro).toFixed(3));
       return futuro;
     }
     if (escolha == 2 || pulo == 2) {
       elemento_pensamento.acao = 2;
       pensamento.push(elemento_pensamento);
       futuro = tax;
-      console.log("A mutação tá boa assim");
-      console.log(futuro);
-      muta = abs(futuro);
+      //console.log("A mutação tá boa assim");
+      //console.log(futuro);
+      muta = parseFloat(abs(futuro).toFixed(3));
       return futuro;
     }
 
 
-
-
-
-
-
   }
-  soma += popula[0].fitness
+  soma += popula[0].fitness;
   return tax;
 }
 
 function mutacao(dna, taxamt) {
-  var tamanho_dna = dna.length;
-  for (i = 0; i < tamanho_dna; i++) {
-    if (random(1) < taxamt) {
+  let tamanho_dna = dna.length;
+  for (let i = 0; i < tamanho_dna; i++) {
+    let tmp = random(1);
+    if (tmp < taxamt) {
       dna = dna.replaceAt(i, String.fromCharCode(random(32, 319))); //Posição, Caractere
     }
   }
@@ -102,19 +101,19 @@ function mutacao(dna, taxamt) {
 
 function criar_pop(n, nome) {
   geracao = 0;
-  var len = nome.length;
+  let len = nome.length;
   geracao += 1;
   quantidade_pop = n;
-  var word;
-  var pessoa = function(gene, fitness) { // Colocar o objeto pessoa no vetor
+  let word;
+  let pessoa = function(gene, fitness) { // Colocar o objeto pessoa no vetor
     this.gene = gene;
     this.fitness = fitness;
   };
 
 
 
-  for (i = 0; i < n; i++) {
-    for (j = 0; j < len; j++) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < len; j++) {
       if (word === undefined) {
         word = String.fromCharCode(random(32, 319));
       } else {
@@ -129,7 +128,7 @@ function criar_pop(n, nome) {
 }
 
 function dynamicSort(property) {
-  var sortOrder = 1;
+  let sortOrder = 1;
   if (property[0] === "-") {
     sortOrder = -1;
     property = property.substr(1);
@@ -141,8 +140,9 @@ function dynamicSort(property) {
 }
 
 function similarity(s1, s2) {
-  var longer = s1;
-  var shorter = s2;
+  let longer = s1;
+  let shorter = s2;
+
   if (s1.length < s2.length) {
     longer = s2;
     shorter = s1;
@@ -154,12 +154,14 @@ function similarity(s1, s2) {
   return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
 }
 
-function editDistance(s1, s2) {
+function editDistance(s1, s2) { // // O()
 
   var costs = new Array();
-  for (var i = 0; i <= s1.length; i++) {
+  let length_s1 = s1.length;
+  let length_s2 = s2.length;
+  for (let i = 0; i <= length_s1; i++) {
     var lastValue = i;
-    for (var j = 0; j <= s2.length; j++) {
+    for (let j = 0; j <= length_s2; j++) {
       if (i == 0)
         costs[j] = j;
       else {
@@ -174,16 +176,16 @@ function editDistance(s1, s2) {
       }
     }
     if (i > 0)
-      costs[s2.length] = lastValue;
+      costs[length_s2] = lastValue;
   }
-  return costs[s2.length];
+  return costs[length_s2];
 }
 
 function fitness() {
   quantidade_pop = popula.length;
-  var count = 0;
   soma_pesos = 0;
   soma_fitness = 0;
+  var count = 0;
   for (i = 0; i < quantidade_pop; i++) {
     count += similarity(popula[i].gene, nome);
     count = (count * 100);
@@ -194,7 +196,7 @@ function fitness() {
     count = 0;
 
   }
-  popula.sort(dynamicSort("-fitness"));
+  popula.sort(dynamicSort("-fitness")); // Quick Sort -> n log n
 }
 
 function checar() {
@@ -207,12 +209,10 @@ function checar() {
     }
   }
 }
-var pai_index = 0;
-var mae_index = 0;
+
 
 function selecionar(soma) {
   peso_aleatorio = int(random(1, soma));
-
   peso_aleatorio2 = int(random(1, soma));
   quantidade_pop = popula.length;
   var next = 0;
@@ -236,7 +236,7 @@ function selecionar(soma) {
 
 
 function procriar() {
-  var temp = new Array;
+  let temp = new Array;
   quantidade_pop = popula.length;
   tamanho_nome = nome.length;
   for (v = 0; v < quantidade_pop; v++) {
