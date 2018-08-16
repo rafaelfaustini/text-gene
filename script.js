b407ba5d5407dc2631566140b1b244478fb39cfc
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-08-15 21:48:05 -0300

    Remodelado
    Com a remodelagem, foi diminuido a dependência de frameworks. Com um design diferente, text gene conta agora com uma performace bem mais estável

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-var elapsed = 0;﻿
-var popula = new Array();
-var pensamento = new Array();
-var geracao = 0;
-var quantidade_pop;
-var tamanho_nome;
-var soma_pesos = 0;
-var soma_fitness = 0;
-var population;
-var nome = "Hello World";
-var Frase = "Hello World";
-var População = 500;
-var PopulaçãoMin = 2;
-var PopulaçãoMax = 2000;
-var myFont;
-var overpass;
-var Mutação = 1;
-var MutaçãoMin = 0;
-var MutaçãoMax = 100;
-var soma = 0;
-var muta = 0.01;
-var pai_index = 0;
-var mae_index = 0;
-var trigger = false;
-var taxa = 0.01;
-
+let elapsed = 0;﻿
+let popula = new Array();
+let pensamento = new Array();
+let geracao = 0;
+let quantidade_pop;
+let tamanho_nome;
+let soma_pesos = 0;
+let soma_fitness = 0;
+let population;
+var nome;
+var mutacao_l = 1;
+let soma = 0;
+let muta = 0.01;
+let pai_index = 0;
+let mae_index = 0;
+let trigger = false;
+let taxa = 0.01;
 
 
 String.prototype.replaceAt = function(index, character) {
@@ -37,11 +28,16 @@ function taxa_dinamica(tax) {
 
   if (tamanho_pensamento > 1 && tamanho_pensamento < (geracao_new) && Number.isInteger(geracao_new)) {
     if ((pensamento[geracao_new - 3].mediafit - pensamento[geracao_new - 2].mediafit) > 0) {
+
+      if(self.lista){
       pulo = self.lista[geracao_new - 2].escolha;
+    } else {
+      console.log('Characters error')
+    }
     }
   }
   if (Number.isInteger(geracao_new) && tamanho_pensamento < geracao_new) {
-    var lembranca = function(acao, mediafit) { // Colocar o objeto pessoa no vetor
+    let lembranca = function(acao, mediafit) { // Colocar o objeto pessoa no vetor
       this.acao = acao;
       this.mediafit = mediafit;
     };
@@ -52,36 +48,32 @@ function taxa_dinamica(tax) {
     soma = 0;
 
     let escolha = int(random(0, 2));
-    
-    if (escolha == 0 || pulo == 0) {
-      elemento_pensamento.acao = 0;
-      pensamento.push(elemento_pensamento);
-      futuro = random(tax - (tax * 2), tax - 0.001);
-    //  console.log("Acho que preciso diminuir a mutação");
-      //console.log(futuro);
-      muta = parseFloat(abs(futuro).toFixed(3));
-      return futuro;
+    switch(escolha){
+      case 0:
+        elemento_pensamento.acao = 0;
+        pensamento.push(elemento_pensamento);
+        futuro = random(tax - (tax * 2), tax - 0.001);
+         //console.log("Acho que preciso diminuir a mutação");
+        //console.log(futuro);
+        muta = parseFloat(abs(futuro).toFixed(3));
+        return futuro.toFixed(3);
+      case 1:
+        elemento_pensamento.acao = 1;
+        pensamento.push(elemento_pensamento);
+        futuro = random(tax + 0.001, tax + (tax * 2));
+        //console.log("Acho que preciso aumentar a mutação");
+        //console.log(futuro);
+        muta = parseFloat(abs(futuro).toFixed(3));
+        return futuro.toFixed(3);
+       case 2:
+        elemento_pensamento.acao = 2;
+        pensamento.push(elemento_pensamento);
+        futuro = tax;
+        //console.log("A mutação tá boa assim");
+        //console.log(futuro);
+        muta = parseFloat(abs(futuro).toFixed(3));
+        return futuro.toFixed(3);
     }
-    if (escolha == 1 || pulo == 1) {
-      elemento_pensamento.acao = 1;
-      pensamento.push(elemento_pensamento);
-      futuro = random(tax + 0.001, tax + (tax * 2));
-      //console.log("Acho que preciso aumentar a mutação");
-      //console.log(futuro);
-      muta = parseFloat(abs(futuro).toFixed(3));
-      return futuro;
-    }
-    if (escolha == 2 || pulo == 2) {
-      elemento_pensamento.acao = 2;
-      pensamento.push(elemento_pensamento);
-      futuro = tax;
-      //console.log("A mutação tá boa assim");
-      //console.log(futuro);
-      muta = parseFloat(abs(futuro).toFixed(3));
-      return futuro;
-    }
-
-
   }
   soma += popula[0].fitness;
   return tax;
@@ -91,8 +83,11 @@ function mutacao(dna, taxamt) {
   let tamanho_dna = dna.length;
   for (let i = 0; i < tamanho_dna; i++) {
     let tmp = random(1);
+    let char = floor(random(63, 122));
+    if (char === 63) char = 32;
+    if (char === 64) char = 46;
     if (tmp < taxamt) {
-      dna = dna.replaceAt(i, String.fromCharCode(random(32, 319))); //Posição, Caractere
+      dna = dna.replaceAt(i, String.fromCharCode(char) ); //Posição, Caractere
     }
   }
   return dna;
@@ -100,6 +95,8 @@ function mutacao(dna, taxamt) {
 
 
 function criar_pop(n, nome) {
+
+
   geracao = 0;
   let len = nome.length;
   geracao += 1;
@@ -189,8 +186,7 @@ function fitness() {
   for (i = 0; i < quantidade_pop; i++) {
     count += similarity(popula[i].gene, nome);
     count = (count * 100);
-    popula[i].fitness = count;
-    popula[i].fitness = pow(popula[i].fitness, 4);
+    popula[i].fitness = pow(count,4);
     soma_fitness += popula[i].fitness;
     soma_pesos += count;
     count = 0;
@@ -201,7 +197,7 @@ function fitness() {
 
 function checar() {
   quantidade_pop = popula.length;
-  for (i = 0; i < quantidade_pop; i++) {
+  for (let i = 0; i < quantidade_pop; i++) {
     if (popula[i].gene == nome) {
       return false;
     } else {
@@ -215,8 +211,8 @@ function selecionar(soma) {
   peso_aleatorio = int(random(1, soma));
   peso_aleatorio2 = int(random(1, soma));
   quantidade_pop = popula.length;
-  var next = 0;
-  var seen = 0;
+  let next = 0;
+  let seen = 0;
   pai_index = 0;
   mae_index = 0;
   for (i = 0; i < quantidade_pop; i++) {
@@ -241,7 +237,7 @@ function procriar() {
   tamanho_nome = nome.length;
   for (v = 0; v < quantidade_pop; v++) {
     selecionar(soma_pesos);
-    append(temp, mutacao(popula[pai_index].gene.substr(0, (tamanho_nome / 2)) + popula[mae_index].gene.substr(tamanho_nome / 2, tamanho_nome), taxa_dinamica(Mutação / 100)))
+    append(temp, mutacao(popula[pai_index].gene.substr(0, (tamanho_nome / 2)) + popula[mae_index].gene.substr(tamanho_nome / 2, tamanho_nome), taxa_dinamica(mutacao_l / 100)))
   }
 
 
@@ -252,7 +248,6 @@ function procriar() {
   }
   temp = [];
   geracao += 1;
-
   fitness();
 
 }
@@ -262,138 +257,114 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function preload() {
-  myFont = loadFont('overpass.otf');
-  overpass = loadFont('overpass-mono-regular.otf')
-}
 
 function tempo() {
   if (trigger == false) {
     elapsed++;
   }
 }
+function reinicia(){
+  popula.length = 0;
+  population = document.getElementById("populacao").value;
+  elapsed = 0;
+  criar_pop(population, nome);
+}
+function frase(obj){
+trigger = false;
+nome = obj.value;
+if(nome == ''){
+  nome="Hello World";
+}
+reinicia();
+
+}
+
+function pop_slider(obj){
+  document.getElementById("pop_label").innerHTML = "População: "+obj.value+" elementos";
+  reinicia();
+
+}
 
 function setup() {
   nome = "Hello World";
 
-  textFont(myFont);
-  if (nome != '') {
-    nome = Frase;
-  } else {
-    nome = "Hello World";
-  }
 
-  if (population < 1) {
-    population = População;
-  }
 
-  criar_pop(population, nome);
+  population = 1000;
+  criar_pop(1000, nome);
   fitness();
 
-  sliderRange(5, 2000, 1);
-  sliderRange(0, 100, 1);
-  gui = createGui('Opções');
-  gui.addGlobals('Frase', 'População', 'Mutação');
   createCanvas(windowWidth, windowHeight);
   setInterval(tempo, 1000);
+  noCanvas();
 
 }
 String.prototype.limite = function(length) {
   return this.length > length ? this.substring(0, length) + "..." : this;
 }
+function keyPressed() {
+    if(trigger){
+      noLoop();
+    } else{
+      loop();
+    }
 
+}
 function draw() {
-  if (nome != Frase && Frase != "") {
-    trigger = false;
-    nome = Frase;
-    popula.length = 0;
-    elapsed = 0;
-    criar_pop(População, nome);
 
-  }
-  if (População != population) {
-    trigger = false;
-    population = População;
-    popula.length = 0;
-    elapsed = 0;
-    criar_pop(População, nome);
-  }
-  if (População != population) {
-    trigger = false;
-    population = População;
-    popula.length = 0;
-    elapsed = 0;
-    criar_pop(População, nome);
-  }
-  if (nome != '') {
-
-    nome = Frase;
-  }
-
-
-  if (População > 1 || Frase == "") {
-    trigger = true;
-  }
-
-
-  background(39, 174, 96);
-  textSize(32);
 
   quantidade_pop = popula.length;
-  quantidade_dinamica = quantidade_pop;
+  let quantidade_dinamica = quantidade_pop;
 
-  var str = "População:\n";
-  switch (População) {
-    case População >= 500 && População < 800:
+  let str = "População:\n";
+  switch (population) {
+    case population >= 500 && population < 800:
       quantidade_dinamica /= 2;
       break;
-    case População >= 800 && População < 1000:
+    case population >= 800 && population < 1000:
       quantidade_dinamica /= 3;
       break;
-    case População >= 1000 && População <= 2000:
+    case population >= 1000 && population <= 2000:
       quantidade_dinamica /= 5;
       break;
   }
-  for (i = 0; i < quantidade_dinamica * 0.4; i++) {
+  for (let i = 0; i < quantidade_dinamica * 0.4; i++) {
 
-    str = str + " " + (popula[i].gene)
+    str += " " + (popula[i].gene)
       .limite(6);
     if (i % 20 == 0 && i != 0)
-      str = str + "\n";
+      str += "\n";
   }
   var tam = str.length;
 
+
   if (popula[0].gene == nome) {
     trigger = true;
+
+
   } else {
     trigger = false;
     procriar();
   }
 
-  var size = 10;
-  while (textWidth(str) + 55 < windowWidth) {
-    size--;
-  }
-  textSize(size);
-  textFont(overpass);
-  text(str, 10, 380);
-  fill(255, 255, 255);
-  textFont(myFont);
-  textSize(64);
+
+
+    document.getElementById("list_popula").innerHTML = str;
+
+
   let atual = (popula[0].gene)
     .limite(25);
-  text(atual, (windowWidth - textWidth(atual)) / 2, 340);
-  textSize(27);
-  var fit = popula[0].fitness / 1000000;
-  text("Geração: " + geracao, 10, 280);
-  text("Maior Fitness: " + (fit)
-    .toFixed(2) + "%", 10, 230);
-  fill(255, 255, 255);
-  var media_fit = (soma_fitness / quantidade_pop) / 1000000;
-  text("Media de Fitness: " + media_fit.toFixed(2) + "%", 10, 255);
-  textSize(27);
-  textSize(20);
-  text('Tempo de Geração: ' + elapsed + ' segundo(s)', 240, 40);
-  text('Mutação Dinâmica: ' + (muta * 100) + '%', 240, 60);
+  document.getElementById("frase").innerHTML = atual
+
+  let fit = popula[0].fitness / 1000000;
+    document.getElementById("geracao").innerHTML = "Geração: " + geracao;
+    document.getElementById("maior_fit").innerHTML = "Maior Fitness: " + (fit)
+      .toFixed(2) + "%";
+
+  let media_fit = (soma_fitness / quantidade_pop) / 1000000;
+  document.getElementById("media_fit").innerHTML = "Media de Fitness: " + media_fit.toFixed(2) + "%"
+document.getElementById("tempo_geracao").innerHTML = 'Tempo de Geração: ' + elapsed + ' segundo(s)';
+document.getElementById("muta_din").innerHTML = 'Mutação Dinâmica: ' + (muta * 100) + '%';
+
 
 }
